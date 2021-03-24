d3d8d480c4dd36ba742140e3949c3712586176e8
Azael Contramaestre <galaxyhhd@hotmail.com>
2021-03-24 19:46:36 -0300

    Slice methoid

---
+++ slice.js
@@ -0,0 +1,13 @@
+// const numbers = [1,2,3,4,5];
+
+
+// const numbers2 = numbers.slice(0);
+
+// console.log(numbers2)
+
+
+const participants = ['Azael', 'Jose', 'Fernanda', 'Bebo']
+
+const winners = participants.slice(0,3);
+
+console.log(winners)
